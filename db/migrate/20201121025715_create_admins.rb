28a601a5adb20f985f6433d21b02d0101dda2d7b
yuto730 <my30.design@gmail.com>
2020-11-28 19:39:10 +0900

    管理画面_実装4

--- db/migrate/20201121025715_create_admins.rb
+++ db/migrate/20201121025715_create_admins.rb
@@ -2,7 +2,6 @@ class CreateAdmins < ActiveRecord::Migration[6.0]
   def change
     create_table :admins do |t|
       t.string :title,       null: false
-      t.text   :description, null: false
       t.references :user,    null: false, foreign_key: true
 
       t.timestamps
