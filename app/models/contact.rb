181832111e7fbc6125059ff46e07f218a5d4ccb7
yuto730 <my30.design@gmail.com>
2020-12-02 17:59:16 +0900

    お問い合わせテーブル_変更

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,6 @@ class Contact < ApplicationRecord
     validates :name
     validates :mail
     validates :mail_confirmation, confirmation: true
-    validates :subject, length: { maximum: 20 }
     validates :message, length: { maximum: 1000 }
   end
 end
