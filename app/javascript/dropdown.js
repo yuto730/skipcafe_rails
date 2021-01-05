afe5a0eb2a3c3a4719b44a03d0d5357d6fa7dede
yuto730 <my30.design@gmail.com>
2021-01-05 17:05:41 +0900

    管理画面の修正

--- app/javascript/dropdown.js
+++ app/javascript/dropdown.js
@@ -1,26 +1,4 @@
 (function () {
-  document.addEventListener('DOMContentLoaded', function() { // HTML解析が終わったら
-    const btn = document.getElementById('dropdown_user_btn'); // ボタンをidで取得
-    if(btn) { // ボタンが存在しないときにエラーになるのを回避
-      btn.addEventListener('click', function(){ //ボタンがクリックされたら
-        this.classList.toggle('is-open'); // is-openを付加する
-      });
-    }
-  });
-}());
-
-(function () {
-  document.addEventListener('DOMContentLoaded', function() {
-    const btn = document.getElementById('dropdown_news_btn');
-    if(btn) {
-      btn.addEventListener('click', function(){
-        this.classList.toggle('is-open');
-      });
-    }
-  });
-}());
-
-(function () {
   document.addEventListener('DOMContentLoaded', function() {
     const btn = document.getElementById('admin_name');
     if(btn) {
